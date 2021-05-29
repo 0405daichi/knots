5c7d106fa03616a1d6263e12a14466a2a7925828
0405daichi <daichi.fukunaga.45@gmail.com>
2021-05-29 18:06:32 +0900

    投稿機能

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,24 @@
 class TopicsController < ApplicationController
   def index
+    @topics = Topic.all
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+    if @topic.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:topic_name, :topic_content)
   end
 end
