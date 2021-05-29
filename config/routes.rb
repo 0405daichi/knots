30f2704112f8e6e04a5e9a6c6b0abbd1b69d0acc
0405daichi <daichi.fukunaga.45@gmail.com>
2021-05-30 05:43:55 +0900

    ユーザー詳細表示機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "topics#index"
-  resources :topics, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :topics
+  resources :users, only: [:show]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_29_092852) do
+ActiveRecord::Schema.define(version: 2021_05_28_122329) do
 
   create_table "topics", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "topic_name", null: false
     t.text "topic_content", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_topics_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -32,4 +34,5 @@ ActiveRecord::Schema.define(version: 2021_05_29_092852) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "topics", "users"
 end
