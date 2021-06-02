2c19f5b4aadf40a5042d169ae1a13dc723dc3444
0405daichi <daichi.fukunaga.45@gmail.com>
2021-06-03 05:21:14 +0900

    投稿関連の単体テスト

--- spec/factories/comment_donations.rb
+++ spec/factories/comment_donations.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :comment_donation do
-    
+    content {'test'}
+    price {10000}
+    token {"tok_abcdefghijk00000000000000000"}
   end
 end
--- spec/models/comment_donation_spec.rb
+++ spec/models/comment_donation_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe CommentDonation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    user = FactoryBot.create(:user)
+    topic = FactoryBot.create(:topic, user_id: user.id)
+    @comment_donation = FactoryBot.build(:comment_donation, user_id: user.id, topic_id: topic.id)
+    sleep 1
+  end
+
+  describe 'コメント投稿' do
+    context 'コメント投稿できるとき' do
+      it '全ての値が正しく入力されていれば保存できる' do
+        expect(@comment_donation).to be_valid
+      end
+    end
+
+    context 'コメント投稿できない時' do
+      it 'contentがないと投稿できない' do
+        @comment_donation.content = ''
+        @comment_donation.valid?
+        expect(@comment_donation.errors.full_messages).to include "Content can't be blank"
+      end
+
+      it 'priceがないと投稿できない' do
+        @comment_donation.price = ''
+        @comment_donation.valid?
+        expect(@comment_donation.errors.full_messages).to include "Price can't be blank"
+      end
+
+      it 'user_idがないと投稿できない' do
+        @comment_donation.user_id = nil
+        @comment_donation.valid?
+        expect(@comment_donation.errors.full_messages).to include "User can't be blank"
+      end
+
+      it 'topic_idがないと投稿できない' do
+        @comment_donation.topic_id = nil
+        @comment_donation.valid?
+        expect(@comment_donation.errors.full_messages).to include "Topic can't be blank"
+      end
+    end
+  end
 end
